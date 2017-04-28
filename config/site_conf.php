3abd8b293383dc6c40eb4fbc9e21dd7ef8e4ee3c
enerjiser2 <Qwerty@)!%>
2017-04-28 09:32:29 +0300

    initial, play with vk api

--- config/site_conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: enerjiser2
- * Date: 17.04.2017
- * Time: 11:34
- */
--- config/vk_conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'client_id' => '5985173',
-    'client_secret' => 'IuMS33cGPxqqHQIxp0f2',
-    'client_service' => '978bd262978bd26297c4771a5297d081f79978b978bd262cf787b51a7931119efce68cc',
-    'redirect_uri' => 'http://testtask/Login/vkAuth'
-];
